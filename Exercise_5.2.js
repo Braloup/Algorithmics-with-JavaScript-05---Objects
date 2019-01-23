7e30979ee2ad1f7193d29627f06a0331079be1b4
VieuxLoupGris <provocemois@gmail.com>
2019-01-23 16:54:27 +0100

    Copy and paste the fonction I£ need to my master fonction

---
+++ Exercise_5.2.js
@@ -0,0 +1,32 @@
+const readLineSync = require("readline-sync");
+
+let askTvSerie = () => {
+
+  let castMembers = [];
+
+  let name = readLineSync.question("Enter the name of your favorite TV serie: ");
+  let productYear = readLineSync.question("Enter the year in which your favorite series was produced: ");
+  let userAnswer;
+  do {
+    let castMember = readLineSync.question("Enter a name of a cast member: ");
+    castMembers.push(castMember);
+    userAnswer = readLineSync.question("Do you want to add any some cast member? (yes or no): ")
+  } while (userAnswer != "no");
+
+  function TvSerie(name, productYear, castMembers) {
+    this.name = name;
+    this.productYear = productYear;
+    this.castMembers = castMembers;
+  }
+
+  let userSerie = new TvSerie(name, productYear, castMembers);
+
+  let userSerieInJson = JSON.stringify(userSerie);
+
+  return(userSerieInJson);
+
+}
+
+console.log(askTvSerie());
+
+let randomizeCast
