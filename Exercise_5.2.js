b6d3ae3b18b3e4889c231abbf1a89cb0f5a0809f
VieuxLoupGris <provocemois@gmail.com>
2019-01-24 09:03:15 +0100

    Mise au propre du code

--- Exercise_5.2.js
+++ Exercise_5.2.js
@@ -6,7 +6,9 @@ let askTvSerie = () => {
 
   let name = readLineSync.question("Enter the name of your favorite TV serie: ");
   let productYear = readLineSync.question("Enter the year in which your favorite series was produced: ");
+  
   let userAnswer;
+
   do {
     let castMember = readLineSync.question("Enter a name of a cast member: ");
     castMembers.push(castMember);
